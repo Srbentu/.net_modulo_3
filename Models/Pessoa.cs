0bab37a8bbc8550a8de564beb28139603f34c13c
Breno Marcomini <breno.marcomini@vitat.com.br>
2023-12-21 17:19:44 -0300

    POO

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -2,5 +2,16 @@ namespace ConsoleApp3.Models;
 
 public class Pessoa
 {
-    
+    public Pessoa(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public void Apresentar()
+    {
+        Console.WriteLine($"Olá, meu nome é {Name}, e tenho {Age} anos.");
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using ConsoleApp3.Models;
+
+var p1 = new Pessoa(name:"Breno", age:33);
+p1.Apresentar();
